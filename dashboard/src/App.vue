49978e0b56181b61d99633dd5a4553e4223340b9
Pelayo Sanchez Margareto <pelaym@amazon.com>
2018-07-05 15:35:08 +0200

    Including first sketch of the quiz game.

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" v-show="status === 'run'">
-    <img src="./assets/logo.png">
+    <img class="logo" src="./assets/logo.png">
     <router-view v-on:state="statusChange" />
   </div>
 </template>
@@ -41,7 +41,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -49,5 +49,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+  .logo {
+    width: 100px;
+  }
 }
 </style>
